92f50e1fd311c3af2594bafd4ed14b2754600c6c
salvatoreDeploy <liderhenrique@gmail.com>
2023-06-11 17:45:04 -0300

    feat: implementation of communication with api through usequery

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -6,6 +6,7 @@ import {
   Heading,
   Icon,
   LightMode,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -21,15 +22,39 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import Pagination from "../../components/Pagination";
 import Link from "next/link";
 import { useEffect } from "react";
+import { useQuery } from "react-query";
 
 export default function ProjectsList() {
-  const isWideVersion = useBreakpointValue({ base: false, lg: true });
+  const { data, isLoading, error } = useQuery(
+    "projects",
+    async () => {
+      const response = await fetch("http://localhost:3000/api/projects");
+      const data = await response.json();
 
-  useEffect(() => {
-    fetch("http://localhost:3000/api/projects")
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  }, []);
+      const projects = data.projects.map((project) => {
+        return {
+          id: project.id,
+          project: project.project,
+          languageUsed: project.languageUsed,
+          startDate: new Date(project.startDate).toLocaleDateString("pt-BR", {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+          }),
+          deliveryDate: new Date(project.deliveryDate).toLocaleDateString(
+            "pt-BR",
+            { day: "2-digit", month: "long", year: "numeric" }
+          ),
+          projectStatus: project.projectStatus,
+        };
+      });
+
+      return projects;
+    },
+    { staleTime: 1000 * 5 } // 5 segundos
+  );
+
+  const isWideVersion = useBreakpointValue({ base: false, lg: true });
 
   return (
     <Box>
@@ -53,87 +78,74 @@ export default function ProjectsList() {
               </Button>
             </Link>
           </Flex>
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.800" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Projeto</Th>
-                <Th>Desenvolvedor</Th>
-                <Th>Linguagem</Th>
-                {isWideVersion && <Th>Data de Inicio</Th>}
-                {isWideVersion && <Th>Data de Termino</Th>}
-                <Th>Status</Th>
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]} color="gray.800" width="8">
-                  <LightMode>
-                    <Checkbox colorScheme="pink" />
-                  </LightMode>
-                </Td>
-                <Td>Dash.Go</Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Henrique Araujo</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      liderhenrique@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                <Td>React</Td>
-                {isWideVersion && <Th>Data de Inicio</Th>}
-                {isWideVersion && <Th>Data de Termino</Th>}
-                <Td>Desenvolvimento</Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    bgColor="purple.500"
-                    leftIcon={<Icon as={RiPencilLine} />}
-                  >
-                    {isWideVersion ? "Editar" : ""}
-                  </Button>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]} color="gray.800" width="8">
-                  <LightMode>
-                    <Checkbox colorScheme="pink" />
-                  </LightMode>
-                </Td>
-                <Td>RentalX</Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Henrique Araujo</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      liderhenrique@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                <Td>Node.Js</Td>
-                {isWideVersion && <Th>Data de Inicio</Th>}
-                {isWideVersion && <Th>Data de Termino</Th>}
-                <Td>Desenvolvimento</Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    bgColor="purple.500"
-                    leftIcon={<Icon as={RiPencilLine} />}
-                  >
-                    {isWideVersion ? "Editar" : ""}
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex>
+              <Text>Não foi possivel obter os dados</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.800" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Projeto</Th>
+                    <Th>Desenvolvedor</Th>
+                    <Th>Linguagem</Th>
+                    {isWideVersion && <Th>Data de Inicio</Th>}
+                    {isWideVersion && <Th>Data de Termino</Th>}
+                    <Th>Status</Th>
+                    <Th width="8"></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.map((project) => {
+                    return (
+                      <>
+                        <Tr key={project.id}>
+                          <Td px={["4", "4", "6"]} color="gray.800" width="8">
+                            <LightMode>
+                              <Checkbox colorScheme="pink" />
+                            </LightMode>
+                          </Td>
+                          <Td>Dash.Go</Td>
+                          <Td>
+                            <Box>
+                              <Text fontWeight="bold">{project.project}</Text>
+                              <Text fontSize="sm" color="gray.300">
+                                liderhenrique@gmail.com
+                              </Text>
+                            </Box>
+                          </Td>
+                          <Td>{project.languageUsed}</Td>
+                          {isWideVersion && <Th>{project.startDate}</Th>}
+                          {isWideVersion && <Th>{project.deliveryDate}</Th>}
+                          <Td>{project.projectStatus}</Td>
+                          <Td>
+                            <Button
+                              as="a"
+                              size="sm"
+                              fontSize="sm"
+                              bgColor="purple.500"
+                              leftIcon={<Icon as={RiPencilLine} />}
+                            >
+                              {isWideVersion ? "Editar" : ""}
+                            </Button>
+                          </Td>
+                        </Tr>
+                      </>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
